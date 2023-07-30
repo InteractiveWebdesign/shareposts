e1787fe1d03769903ad624b673824708fd17debe
InteractiveWebdesign <ruud.weijters@outlook.com>
2023-07-30 18:21:48 +0200

    26-database en opzet voor OOP php applicatie SharePosts

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,7 +5,7 @@ class Pages extends Controller {
 
     public function index() {
         $data = [
-        'title' => 'RW-MVC',
+        'title' => 'SharePosts',
       ];
 
       $this->view('pages/index', $data);
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,3 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <h1><?php echo $data['title']; ?></h1>
-<p>This is the RW-MVC PHP Framework. Please refer to the docs for more information:</p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
