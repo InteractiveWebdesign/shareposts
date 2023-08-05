1bf626548bde3dee3caf1485aa13535c9eb23fb3
InteractiveWebdesign <ruud.weijters@outlook.com>
2023-08-05 18:46:40 +0200

    les 39 add form for create post funct

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,6 +4,10 @@ class Pages extends Controller {
     }
 
     public function index() {
+        if(isLoggedIn()){
+            redirect('posts');
+        }
+
         $data = [
         'title' => 'SharePosts',
         'description' => 'Simple social network built on the OOP MVC PHP Framework'
