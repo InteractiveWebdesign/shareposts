a13a9340c906f808aee559d2681c7591f7b1501f
InteractiveWebdesign <ruud.weijters@outlook.com>
2023-08-05 12:56:35 +0200

    39 Post Model and Diplays: show data from the database on page

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -4,11 +4,20 @@ class Posts extends Controller {
     public function __construct(){
         if(!isLoggedIn()){
             redirect('users/login');
+    
         }
+
+        $this->postModel = $this->model('Post');
     }
 
     public function index(){
-        $data = [];
+        // Get Posts
+        $posts = $this->postModel->getPosts();
+
+        $data = [
+            'posts' => $posts
+        ];
+
         $this->view('posts/index', $data);
     }
 }
