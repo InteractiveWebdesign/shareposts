a41deeab0c4d1e44784e14b20438ac68a94158d9
InteractiveWebdesign <ruud.weijters@outlook.com>
2023-08-06 11:31:44 +0200

    les 41 show post detail page added view and update and delete buttons

--- app/models/User.php
+++ app/models/User.php
@@ -55,4 +55,16 @@
         return false;
       }
     }
+
+     // Get user by ID
+     public function getUserById($id){
+      
+      $this->db->query('SELECT * FROM users WHERE id = :id');
+      $this->db->bind(':id', $id);
+
+      $row = $this->db->single();
+
+      // Check row
+      return $row;
+    }
   }
